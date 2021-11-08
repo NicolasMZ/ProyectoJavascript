2cae6275ed5dd7d9299a308eb6ca44b9c3ce9259
NicolasMZ <nmzorzini@gmail.com>
2021-11-07 23:22:10 -0300

    Creada clase marco. Funciones transformadas en métodos de la misma.

--- js/script.js
+++ js/script.js
@@ -13,63 +13,78 @@ function promptNumber(message = "", integerOnly = false, positiveOnly = false){
     return value;
 }
 
-function precioMetroLineal(){
-    switch(inputForma){
-        case '1': return 100*inputAnchoVarilla;
-        case '2': return 150*inputAnchoVarilla;
-        case '3': return 200*inputAnchoVarilla;
+class marco {
+    constructor(ancho,alto){
+        this.anchoMarco = ancho;
+        this.altoMarco = alto;
+        this.formaVarilla = 'plana';
+        this.anchoVarilla = 1;
+        this.conVidrio = false;
+        this.conTapa = false;
+        this.conEspejo = false;
+        this.conPptBco = false;
+        this.conPptCol = false;
+    }
+
+    precioMetroLineal(){
+        switch(this.formaVarilla){
+            case 'plana': return 100*this.anchoVarilla;
+            case 'bombé': return 150*this.anchoVarilla;
+            case 'italiana': return 200*this.anchoVarilla;
+        }
+    }
+    
+    precioMetroCuadrado(){
+        return (
+        this.conVidrio*2500 + 
+        this.conTapa*1500 + 
+        this.conEspejo*4400 + 
+        this.conPptBco*1300 + 
+        this.conPptCol*2000)
+    }
+    
+
+    mostrarResultado(){
+        let resultado = `Presupuesto por marco medida ${this.anchoMarco} por ${this.altoMarco} con varilla ${this.formaVarilla} de ${this.anchoVarilla} cm.`
+        if(this.conVidrio){resultado = resultado + ", con vidrio"};
+        if(this.conTapa){resultado = resultado + ", con tapa"};
+        if(this.conEspejo){resultado = resultado + ", con espejo"};
+        if(this.conPptBco){resultado = resultado + ", con paspartú blanco"};
+        if(this.conPptCol){resultado = resultado + ", con paspartú de color"};
+
+        let precioLineal = (this.anchoMarco + this.altoMarco)/50 * this.precioMetroLineal();
+        let precioArea = inputAltoMarco*inputAnchoMarco/10000 * this.precioMetroCuadrado();
+
+        alert(resultado + `.\rEl precio final es de $ ${precioLineal+precioArea}`)
     }
 }
 
-function precioMetroCuadrado(){
-    return (
-    inputVidrio*2500 + 
-    inputTapa*1500 + 
-    inputEspejo*4400 + 
-    inputPptBco*1300 + 
-    inputPptCol*2000)
-}
+let inputAnchoMarco = promptNumber("Ingrese ancho del marco en centímetros",false,true);
+let inputAltoMarco = promptNumber("Ingrese alto del marco en centímetros",false,true);
+const marco1 = new marco(inputAnchoMarco, inputAltoMarco);
 
-function mostrarResultado(){
-    let resultado = `Presupuesto por marco medida ${inputAnchoMarco} por ${inputAltoMarco} con varilla `
-    switch(inputForma){
-        case '1':
-            resultado = resultado + "plana";
-            break;
-        case '2':
-            resultado = resultado + "bombé";
-            break;
-        case '3':
-            resultado = resultado + "italiana";
-            break;}
-    resultado = resultado + ` de ${inputAnchoVarilla} cm.`;
-    if(inputVidrio){resultado = resultado + ", con vidrio"};
-    if(inputTapa){resultado = resultado + ", con tapa"};
-    if(inputEspejo){resultado = resultado + ", con espejo"};
-    if(inputPptBco){resultado = resultado + ", con paspartú blanco"};
-    if(inputPptCol){resultado = resultado + ", con paspartú de color"};
-    alert(resultado + `.\rEl precio final es de $ ${precioLineal+precioArea}`)
-}
-
-let inputAnchoMarco;
-let inputAltoMarco;
 let inputForma;
-let inputAnchoVarilla;
-
-inputAnchoMarco = promptNumber("Ingrese ancho del marco en centímetros",false,true);
-inputAltoMarco = promptNumber("Ingrese alto del marco en centímetros",false,true);
-
 do {inputForma = prompt("Ingrese forma de varilla\r1. Plana\r2. Bombé\r3. Italiana");}
 while (inputForma != 1 && inputForma != 2 && inputForma != 3)
-inputAnchoVarilla = promptNumber("Ingrese ancho de la varilla",true,true);
+switch(inputForma){
+    case '1':
+        inputForma = "plana";
+        break;
+    case '2':
+        inputForma = "bombé";
+        break;
+    case '3':
+        inputForma = "italiana";
+        break;
+}
+marco1.formaVarilla = inputForma;
 
-let inputVidrio = window.confirm("¿El marco lleva vidrio?");
-let inputTapa = window.confirm("¿El marco lleva tapa?");
-let inputEspejo = window.confirm("¿El marco lleva espejo?");
-let inputPptBco = window.confirm("¿El marco lleva paspartú blanco?");
-let inputPptCol = window.confirm("¿El marco lleva paspartú de color?");
+marco1.anchoVarilla = promptNumber("Ingrese ancho de la varilla",true,true);
+marco1.conVidrio = window.confirm("¿El marco lleva vidrio?");
+marco1.conTapa = window.confirm("¿El marco lleva tapa?");
+marco1.conEspejo = window.confirm("¿El marco lleva espejo?");
+marco1.conPptBco = window.confirm("¿El marco lleva paspartú blanco?");
+marco1.conPptCol = window.confirm("¿El marco lleva paspartú de color?");
 
-let precioLineal = (inputAltoMarco + inputAnchoMarco)/50 * precioMetroLineal();
-let precioArea = inputAltoMarco*inputAnchoMarco/10000 * precioMetroCuadrado();
-
-mostrarResultado();
+marco1.mostrarResultado();
+console.log(marco1);
